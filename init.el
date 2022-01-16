4e4fd08e27ad26ec364d06c0df35d104eda1fd46
Thomas Freeman <tfree87@users.noreply.github.com>
2022-01-15 21:05:03 -0500

    Doomified my Emacs
    Added a bunch of changes:
    1. Changed the theme to Doom
    2. Going to try out tab bar for a while
    3. Added treemacs
    4. Cleaned up some bad code
    5. Enable nyan-mode animation by default
    6. Turn of tab line

--- init.el
+++ init.el
@@ -26,13 +26,14 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(unless (package-installed-p
-         'use-package)
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
+(use-package benchmark-init
+  :ensure t
+  :hook
+  (after-init . benchmark-init/deactivate))
 
 (use-package emacs
   :custom
@@ -51,15 +52,10 @@
   (display-time-mode 1)
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (toggle-scroll-bar -1))
+  (toggle-scroll-bar -1)
+  (tab-bar-mode))
 
 (load custom-file)
-
-(use-package benchmark-init
-  :disabled t
-  :ensure t
-  :hook
-  (after-init . benchmark-init/deactivate))
 
 (use-package all-the-icons
   :if (and window-system (not (file-exists-p "~/runemacs.bat")))
@@ -89,7 +85,18 @@
   :config
   (all-the-icons-ivy-rich-mode 1))
 
+(use-package doom-themes
+  :if window-system
+  :ensure t
+  :init (load-theme 'doom-vibrant))
+
+(use-package doom-modeline
+  :if window-system
+  :ensure t
+  :init (doom-modeline-mode))
+
 (use-package spacemacs-theme
+  :disabled t
   :if window-system
   :ensure t
   :defer t
@@ -97,6 +104,7 @@
 
 (use-package spaceline
   :if window-system
+  :disabled t
   :ensure t
   :config  
   (require 'spaceline-config)
@@ -109,7 +117,8 @@
   :custom
   (nyan-wavy-trail t)
   :config
-  (nyan-mode))
+  (nyan-mode)
+  (nyan-start-animation))
 
 (use-package c-mode
   :mode ("\\.c\\'"
@@ -737,33 +746,102 @@
   :defer t
   :after org)
 
-(global-tab-line-mode t)
+(use-package treemacs
+  :after treemacs-all-the-icons
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (treemacs-load-theme "all-the-icons")
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                5000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
 
-(setq tab-line-new-button-show nil)  ;; do not show add-new button
-(setq tab-line-close-button-show nil)  ;; do not show close button
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
 
-(setq tab-line-separator "")  ;; set it to empty
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
 
-(defvar my/tab-height 22)
-(defvar my/tab-left (powerline-wave-right 'tab-line nil my/tab-height))
-(defvar my/tab-right (powerline-wave-left nil 'tab-line my/tab-height))
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
 
-(defun my/tab-line-tab-name-buffer (buffer &optional _buffers)
-  (powerline-render (list my/tab-left
-                          (format "%s" (buffer-name buffer))
-                          my/tab-right)))
-(setq tab-line-tab-name-function #'my/tab-line-tab-name-buffer)
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
-;; tab color settings
-(set-face-attribute 'tab-line-tab nil ;; active tab in another window
-      :inherit 'tab-line
-      :foreground "gray" :background "#5d4d7a" :box nil)
-(set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
-      :background "#EEAD0E" :foreground "white" :box nil)
-(set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
-      :background "#373040" :foreground "gray" :box nil)
-(set-face-attribute 'tab-line-highlight nil ;; mouseover
-      :background "white" :foreground "black")
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-all-the-icons
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
 
 (use-package tramp
   :config
