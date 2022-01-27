d1223f4b8b8262872ab670369f86040bfb9a544e
Thomas Freeman <tfree87@users.noreply.github.com>
2022-01-27 10:39:05 -0500

    Added comments to runemacs.bat
    Added comments to runemacs.bat for user documentation.

--- runemacs.bat
+++ runemacs.bat
@@ -1,2 +1,10 @@
+rem runemacs.bat - A script to launch Emacs from a flash drive
+rem Author : Thomas Freeman
+rem Time-stamp: <2022-01-27>
+
+
+rem Set the Emacs home directory to the drive label of the flash drive.
 set HOME=%~dp0
-"%~dp0/emacs/bin/runemacs.exe" %*
+
+rem Find the Emacs executable and execute it.
+"%~dp0/emacs/bin/runemacs.exe" %
