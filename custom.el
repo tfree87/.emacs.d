06fa7667a76b11c3b24fc36a9fa9cfc64a19c327
Thomas Freeman <tfree87@users.noreply.github.com>
2022-02-04 17:41:52 -0500

    Changed faces in Sunrise commander to make it easier to read

--- custom.el
+++ custom.el
@@ -16,4 +16,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(sr-active-path-face ((t (:foreground "yellow" :weight bold :height 120))))
+ '(sr-highlight-path-face ((t (:background "yellow" :foreground "dark violet" :weight bold :height 120))))
+ '(sr-passive-path-face ((t (:foreground "lightgray" :weight bold :height 120)))))
