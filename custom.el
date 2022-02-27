9a68c787850e7f108d1e2d2e6716ba3cfde2700e
Thomas Freeman <tlfree87@gmail.com>
2022-02-26 20:44:39 -0500

    Changed default font

--- custom.el
+++ custom.el
@@ -3,14 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-safe-themes
    '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
+ '(display-time-mode t)
+ '(menu-bar-mode nil)
  '(safe-local-variable-values
    '((org-use-tag-inheritance)
      (eval add-hook 'after-save-hook
 	   (lambda nil
 	     (org-babel-tangle))
-	   nil t))))
+	   nil t)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
