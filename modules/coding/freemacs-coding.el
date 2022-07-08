f9fb119eb6eb33e360654ac85c137db54edb3a74
tfree76 <tlfree87@gmail.com>
2022-07-08 08:45:27 -0400

    Move treemacs-all-the-icons to freemacs-alltheicons module

--- modules/coding/freemacs-coding.el
+++ modules/coding/freemacs-coding.el
@@ -78,7 +78,6 @@
   (treemacs-fringe-indicator-mode 'always)
   (treemacs-hide-gitignored-files-mode nil)
   :config
-  (treemacs-load-theme "all-the-icons")
   (pcase (cons (not (null (executable-find "git")))
                (not (null treemacs-python-executable)))
     (`(t . t)
@@ -95,10 +94,6 @@
         ("C-x t C-t" . treemacs-find-file)
         ("C-x t M-t" . treemacs-find-tag)))
 
-(use-package treemacs-all-the-icons
-  :after treemacs
-  :straight t
-  :config (treemacs-load-theme 'all-the-icons))
 
 (use-package treemacs-magit
   :after (treemacs magit)
--- modules/theme/freemacs-alltheicons.el
+++ modules/theme/freemacs-alltheicons.el
@@ -2,6 +2,14 @@
   :straight t
   :defer 3)
 
+(use-package all-the-icons-completion
+  :straight t
+  :after all-the-icons
+  :config
+  (all-the-icons-completion-mode)
+  (when (boundp 'marginalia-mode)
+    (add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)))
+
 (use-package all-the-icons-dired
   :straight t
   :after all-the-icons
@@ -13,13 +21,10 @@
   :after all-the-icons
   :hook (ibuffer-mode . all-the-icons-ibuffer-mode))
 
-(use-package all-the-icons-completion
+(use-package treemacs-all-the-icons
+  :after treemacs
   :straight t
-  :after all-the-icons
-  :config
-  (all-the-icons-completion-mode)
-  (when (boundp 'marginalia-mode)
-    (add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)))
+  :config (treemacs-load-theme 'all-the-icons))
 
 (provide 'freemacs-alltheicons)
 
