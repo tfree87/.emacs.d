082fcca1df7a0a359dc48a63aacd6b3305656e8d
tfree76 <tlfree87@gmail.com>
2022-07-10 17:18:20 -0400

    Add All the icons support to Citar

--- modules/publishing/freemacs-academic-writing.el
+++ modules/publishing/freemacs-academic-writing.el
@@ -7,9 +7,27 @@
                    :repo "emacs-citar/citar"
                    :branch "main")
   :defer t
-  :after org
+  :after org oc
   :custom
-  (citar-bibliography org-cite-global-bibliography))
+  (citar-bibliography org-cite-global-bibliography)
+  (citar-library-paths '("~/bib/files"))
+  :config
+  (with-eval-after-load "all-the-icons"
+    (setq citar-symbols
+          `((file ,(all-the-icons-faicon "file-o" :face 'all-the-icons-green :v-adjust -0.1) . " ")
+            (note ,(all-the-icons-material "speaker_notes" :face 'all-the-icons-blue :v-adjust -0.3) . " ")
+            (link ,(all-the-icons-octicon "link" :face 'all-the-icons-orange :v-adjust 0.01) . " ")))
+    (setq citar-symbol-separator "  "))
+  (setq citar-file-parser-functions
+        '(citar-file--parser-default
+          citar-file--parser-triplet)))
+
+(add-to-list 'savehist-additional-variables 'citar-history)
+
+(use-package citar-embark
+  :after citart embark
+  :no-require t
+  :config (citar-embark-mode))
 
 (use-package oc
   :defer t
