b37af7c35cf36d1ae61cfb523b7d4724e4d9e2b2
tfree76 <tlfree87@gmail.com>
2022-07-10 17:18:50 -0400

    Add multi-vterm
    Multi-vterm makes it possible to open multiple vterm sessions in Emacs

--- modules/shells/freemacs-vterm.el
+++ modules/shells/freemacs-vterm.el
@@ -1,5 +1,13 @@
 (use-package vterm
+  :straight nil
   :defer t)
+
+(use-package multi-vterm
+  :straight nil
+  :defer t
+  :commands (multi-vterm
+             multi-vterm-project
+             multi-vterm-dedicated-toggle))
 
 (provide 'freemacs-vterm)
 
