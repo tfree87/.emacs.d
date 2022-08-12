ea6ed5794ab45ee654d52d1bf1c16039dca952b2
Thomas Freeman - Network <freeman_thomas@network.lilly.com>
2022-08-11 20:23:56 -0400

    Move org-contacts to a separate modudule
    Org-contacts was moved to a separate module and installed separately
    using straight as it no longer seems to be part of org-contrib.

---
+++ modules/email/freemacs-contacts.el
@@ -0,0 +1,10 @@
+(use-package org-contacts
+  :straight t
+  :after org
+  :custom
+  (org-contacts-files '("~/gtd/contacts.org"
+                        "~/gtd/contacts2.org")))
+
+(provide 'freemacs-contacts)
+
+;;; freemacs-contacts.el ends here
