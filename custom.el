919e39526a3926a0497cad9a152e4656ff9de9bf
Thomas <thomas@pidesktop>
2024-09-29 10:15:59 -0400

    Add minimal custom.el to prevent custeom.el missing error

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" default))
+ '(org-safe-remote-resources
+   '("\\`https://tfree87\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight normal :height 100 :width normal))))
+ '(fixed-pitch ((t (:family "Hack"))))
+ '(sr-active-path-face ((t (:foreground "yellow" :weight bold :height 120))))
+ '(sr-highlight-path-face ((t (:background "yellow" :foreground "dark violet" :weight bold :height 120))))
+ '(sr-passive-path-face ((t (:foreground "lightgray" :weight bold :height 120))))
+ '(variable-pitch ((t (:family "Liberation Serif")))))
