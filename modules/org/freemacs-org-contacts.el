d8391ac1dba5163fda4220d9e4fda35e03a468a4
Thomas <thomas@pidesktop>
2024-10-09 21:22:37 -0400

    Update freemacs-org-contacts configuration

---
+++ modules/org/freemacs-org-contacts.el
@@ -0,0 +1,7 @@
+(use-package org-contacts
+  :straight (:host github :repo "tfree87/org-contacts"
+                     :branch "master")
+  :custom
+  (org-contacts-files '("~/org/contacts.org")))
+
+(provide 'freemacs-org-contacts)
