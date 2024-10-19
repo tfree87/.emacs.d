0648b4727727ef5b283c0ee941c4635dbb636433
Thomas <thomas@pidesktop>
2024-10-19 19:33:17 -0400

    Add freemacs-cwm

---
+++ modules/theme/freemacs-cwm.el
@@ -0,0 +1,9 @@
+(use-package centered-window
+  :straight t
+  :custom
+  (cwm-incremental-padding t)
+  (cwm-incremental-padding-% 5)
+  :config
+  (centered-window-mode t))
+
+(provide 'freemacs-cwm)
