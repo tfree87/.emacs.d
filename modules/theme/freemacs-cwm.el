30b690bc073e304d8b5e8610a25057e1f2310f41
Thomas <thomas@pidesktop>
2024-10-21 22:17:18 -0400

    Make centered-window mode a feature that must be toggled.

--- modules/theme/freemacs-cwm.el
+++ modules/theme/freemacs-cwm.el
@@ -1,10 +1,12 @@
-(use-package centered-window
-  :straight t
-  :custom
-  (cwm-incremental-padding t)
-  (cwm-incremental-padding-% 5)
-  (cwm-centered-window-width 1000)
-  :config
-  (centered-window-mode t))
+(setq-default left-margin-width 3 right-margin-width 3)
+
+;  (use-package centered-window
+;      :straight t
+;      :custom
+;      (cwm-incremental-padding t)
+;      (cwm-incremental-padding-% 5)
+;      (cwm-centered-window-width 1000)
+;      :config
+;      (centered-window-mode t))
 
 (provide 'freemacs-cwm)
