66695556067ca92526ae4e77617f5294853feebd
Thomas <thomas@pidesktop>
2024-10-26 19:45:20 -0400

    Add eat module

---
+++ modules/shells/freemacs-eat.el
@@ -0,0 +1,14 @@
+(use-package eat
+  :straight (:type git
+                   :host codeberg
+                   :repo "akib/emacs-eat"
+                   :files ("*.el" ("term" "term/*.el") "*.texi"
+                           "*.ti" ("terminfo/e" "terminfo/e/*")
+                           ("terminfo/65" "terminfo/65/*")
+                           ("integration" "integration/*")
+                           (:exclude ".dir-locals.el" "*-tests.el")))
+  :defer t
+  :hook
+  (eshell-load . eat-eshell-mode))
+
+(provide 'freemacs-eat)
