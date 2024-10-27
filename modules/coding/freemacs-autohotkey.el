f5d3da33c559edab3195a0a8847a5a9c5f78561b
Thomas <thomas@pidesktop>
2024-10-26 20:27:47 -0400

    Correct typo

--- modules/coding/freemacs-autohotkey.el
+++ modules/coding/freemacs-autohotkey.el
@@ -1,15 +1,15 @@
-;; Install AJK Mode
+;; Install AHK Mode
 
 ;; Install AHK Mode with Straight.
 
 
-;; [[file:freemacs-autohotkey.org::*Install AJK Mode][Install AJK Mode:1]]
+;; [[file:freemacs-autohotkey.org::*Install AHK Mode][Install AHK Mode:1]]
 (use-package ahk-mode
   :straight t
   :defer t
   :custom
   <<ahk_custom>>)
-;; Install AJK Mode:1 ends here
+;; Install AHK Mode:1 ends here
 
 (provide 'freemacs-autohotkey)
 
