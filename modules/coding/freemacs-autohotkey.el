cf70870f90478af03c9fabfdc4c6220bbd6f7d68
Thomas <thomas@pidesktop>
2024-10-26 20:27:13 -0400

    Update documentation on AHK mode

--- modules/coding/freemacs-autohotkey.el
+++ modules/coding/freemacs-autohotkey.el
@@ -1,8 +1,15 @@
+;; Install AJK Mode
+
+;; Install AHK Mode with Straight.
+
+
+;; [[file:freemacs-autohotkey.org::*Install AJK Mode][Install AJK Mode:1]]
 (use-package ahk-mode
   :straight t
   :defer t
   :custom
-  (ahk-indentation 2))
+  <<ahk_custom>>)
+;; Install AJK Mode:1 ends here
 
 (provide 'freemacs-autohotkey)
 
