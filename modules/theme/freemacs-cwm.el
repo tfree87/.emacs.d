ff985763b0abef3ea8bbdc67c811e9adec1341fb
Thomas <thomas@pidesktop>
2024-11-04 18:35:22 -0500

    Moved default margin out of cwm module

--- modules/theme/freemacs-cwm.el
+++ modules/theme/freemacs-cwm.el
@@ -1,12 +1,15 @@
-(setq-default left-margin-width 3 right-margin-width 3)
+;; Install Centered Window Mode
 
-;  (use-package centered-window
-;      :straight t
-;      :custom
-;      (cwm-incremental-padding t)
-;      (cwm-incremental-padding-% 5)
-;      (cwm-centered-window-width 1000)
-;      :config
-;      (centered-window-mode t))
+;; Toggle on centered window mode when using a large screen with only a single Emacs window.
+
+
+;; [[file:freemacs-cwm.org::*Install Centered Window Mode][Install Centered Window Mode:1]]
+(use-package centered-window
+  :straight t
+  :custom
+  (cwm-incremental-padding t)
+  (cwm-incremental-padding-% 10)
+  (cwm-centered-window-width 1000))
+;; Install Centered Window Mode:1 ends here
 
 (provide 'freemacs-cwm)
