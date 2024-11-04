21efffc54b95e8bc17934a72b15eb6f8f0f71cb4
Thomas <thomas@pidesktop>
2024-11-04 18:34:26 -0500

    Moved default margin setting to defaults module

--- modules/defaults/freemacs-defaults.el
+++ modules/defaults/freemacs-defaults.el
@@ -1,14 +1,14 @@
 (use-package emacs
   :custom
   (bidi-paragraph-direction 'left-to-right)
-  (delete-by-moving-to-trash t)
   (version-control t)
   (delete-old-versions t)
   (vc-make-backup-files t)
-  (inhibit-startup-screen t)
+  ;  (inhibit-startup-screen t)
   (register-preview-delay 0)
   (ring-bell-function #'ignore)
   (visible-bell t)
+  (delete-by-moving-to-trash t)
   (column-number-mode t)
   :config
   (add-hook 'before-save-hook 'time-stamp)
@@ -16,6 +16,15 @@
       (setq bidi-inhibit-bpa t))
   (setq-default indent-tabs-mode nil)
   (fset 'yes-or-no-p 'y-or-n-p))
+
+;; Default Margin
+
+;; Use a default left and right margin of 3 to help clean up the look of Emacs buffers near window edges.
+
+
+;; [[file:freemacs-defaults.org::*Default Margin][Default Margin:1]]
+(setq-default left-margin-width 3 right-margin-width 3)
+;; Default Margin:1 ends here
 
 (use-package battery
   :straight (:type built-in)
