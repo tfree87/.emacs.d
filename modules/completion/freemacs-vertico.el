e19ce8ba79f1a8f6530892843fbe4cbed4fb1a5f
tfree87 <tlfree87@gmail.com>
2024-11-08 08:13:28 -0500

    Updated Vertico mode keybinds

--- modules/completion/freemacs-vertico.el
+++ modules/completion/freemacs-vertico.el
@@ -1,10 +1,25 @@
+;; Vertico
+
+;; [[https://github.com/minad/vertico][Vertico]] removes the ~*Completions*~ buffer and instead, populates a vertical list of completion options in the minibuffer region.
+
 (use-package vertico
   :straight t
   :demand t
+  :bind
+  (:map vertico-map
+        ("C-j" . vertico-next)
+        ("C-k" . vertico-previous)
+        ("C-f" . vertico-exit)
+        :map minibuffer-local-map
+        ("M-b" . backward-kill-word))
   :custom
   (vertico-cycle t)
   (vertico-resize t)
   :init
   (vertico-mode))
+
+;; End
+
+;; Tell Emacs what package this file provides.
 
 (provide 'freemacs-vertico)
