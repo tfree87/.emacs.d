d1f16bf64c080cb8824ee492e601e1b8817063eb
Thomas <thomas@pidesktop>
2024-11-10 19:58:08 -0500

    Add listen module

---
+++ modules/multimedia/freemacs-listen.el
@@ -0,0 +1,11 @@
+;; Install Listen.el
+
+
+;; [[file:freemacs-listen.org::*Install Listen.el][Install Listen.el:1]]
+(use-package listen
+  :straight t
+  :custom
+  (listen-directory "~/Music/"))
+;; Install Listen.el:1 ends here
+
+(provide 'freemacs-listen)
