05744e6c3d6cb25494d9846547225fca0c5404a0
Thomas <thomas@pidesktop>
2024-11-12 20:06:37 -0500

    Merge changes

--- custom.el
+++ custom.el
@@ -6,7 +6,12 @@
  '(custom-safe-themes
    '("9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" default))
  '(org-safe-remote-resources
-   '("\\`https://tfree87\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'")))
+   '("\\`https://tfree87\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+           (lambda nil
+             (org-babel-tangle))
+           nil t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,6 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight normal :height 100 :width normal))))
  '(fixed-pitch ((t (:family "Hack"))))
+ '(fringe ((t (:background "#0c0a20"))))
  '(sr-active-path-face ((t (:foreground "yellow" :weight bold :height 120))))
  '(sr-highlight-path-face ((t (:background "yellow" :foreground "dark violet" :weight bold :height 120))))
  '(sr-passive-path-face ((t (:foreground "lightgray" :weight bold :height 120))))
