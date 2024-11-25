a809d920442d162b35bb6baed218f2894fbccb64
Thomas <thomas@pidesktop>
2024-11-24 21:16:09 -0500

    Update documentation in server module

--- modules/server/freemacs-server.el
+++ modules/server/freemacs-server.el
@@ -1,3 +1,7 @@
+;; Start Emacs Server
+
+;; I have this option run only when I am not executing Emacs as a portable app as it is unlikely that there will be Emacs installed on the system that I am running Emacs on when I am using it as a portable application.
+
 ;; Start an Emacs server
 
 (use-package server
@@ -6,5 +10,9 @@
   :config
   (unless (server-running-p)
     (server-start)))
+
+;; End
+
+;; Tell Emacs what feature this file provides.
 
 (provide 'freemacs-server)
