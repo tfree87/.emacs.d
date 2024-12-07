7a4199a559b0661bcb2b5ed68831c394013df033
Thomas <thomas@pidesktop>
2024-12-07 10:48:14 -0500

    Cleaned up elfeed - only using elfeed-protocol

--- modules/elfeed/freemacs-elfeed.el
+++ modules/elfeed/freemacs-elfeed.el
@@ -1,26 +1,7 @@
 (use-package elfeed
   :straight t
   :defer t
-  :commands elfeed
-  :custom
-  (setq elfeed-feeds
-        '("https://www.sciencedaily.com/rss/all.xml")))
-
-(use-package elfeed-tube
-  :straight (elfeed-tube :host github
-                         :repo "karthink/elfeed-tube"
-                         :branch "master")
-  :defer t
-  :after elfeed
-  :config
-  (require 'elfeed-tube-mpv)
-  (elfeed-tube-setup)
-  (elfeed-tube-add-feeds '("https://www.youtube.com/c/inanutshell/"
-                           "https://www.youtube.com/c/veritasium/")))
-
-(use-package mpv
-  :straight t
-  :defer t)
+  :commands elfeed)
 
 (provide 'freemacs-elfeed)
 
