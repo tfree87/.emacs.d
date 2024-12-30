90b67490135431e7a9b44a110d620d1264bc20ba
Thomas <thomas@pidesktop>
2024-12-30 17:18:55 -0500

    Add holiday-sexp for Day after Thanksgiving

--- custom.el
+++ custom.el
@@ -5,6 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" default))
+ '(holiday-local-holidays
+   '((holiday-sexp
+      '(let
+           ((date
+             (calendar-nth-named-day 4 4 11 year)))
+         (setf
+          (nth 1 date)
+          (1+
+           (nth 1 date)))
+         date)
+      "Day after Thanksgiving")))
  '(org-safe-remote-resources
    '("\\`https://tfree87\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
  '(safe-local-variable-values
@@ -20,7 +31,22 @@
  '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight normal :height 100 :width normal))))
  '(fixed-pitch ((t (:family "Hack"))))
  '(fringe ((t (:background "#0c0a20"))))
- '(sr-active-path-face ((t (:foreground "yellow" :weight bold :height 120))))
- '(sr-highlight-path-face ((t (:background "yellow" :foreground "dark violet" :weight bold :height 120))))
- '(sr-passive-path-face ((t (:foreground "lightgray" :weight bold :height 120))))
- '(variable-pitch ((t (:family "Liberation Serif")))))
+ '(header-line ((t :box (:line-width 4 :color "#0a091c" :style nil))))
+ '(header-line-highlight ((t :box (:color "#f2f3f7"))))
+ '(keycast-key ((t)))
+ '(line-number ((t :background "#0c0a20")))
+ '(mode-line ((t :box (:line-width 6 :color "#0a091c" :style nil))))
+ '(mode-line-active ((t :box (:line-width 6 :color "#0a091c" :style nil))))
+ '(mode-line-highlight ((t :box (:color "#f2f3f7"))))
+ '(mode-line-inactive ((t :box (:line-width 6 :color "#0c0a20" :style nil))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "#0c0a20" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "#090819" :style nil))))
+ '(tab-line-tab ((t)))
+ '(tab-line-tab-active ((t)))
+ '(tab-line-tab-current ((t)))
+ '(tab-line-tab-inactive ((t)))
+ '(variable-pitch ((t (:family "Liberation Serif"))))
+ '(vertical-border ((t :background "#0c0a20" :foreground "#0c0a20")))
+ '(window-divider ((t (:background "#0c0a20" :foreground "#0c0a20"))))
+ '(window-divider-first-pixel ((t (:background "#0c0a20" :foreground "#0c0a20"))))
+ '(window-divider-last-pixel ((t (:background "#0c0a20" :foreground "#0c0a20")))))
