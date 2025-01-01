1506d416092208586c2f05d37a5fa8cc666e6dd6
Thomas <thomas@pidesktop>
2025-01-01 15:26:32 -0500

    Fix tangling issue with Straight.el.

--- modules/package-management/freemacs-straight.el
+++ modules/package-management/freemacs-straight.el
@@ -1,27 +1,3 @@
-;; # -*-eval: (add-hook 'after-save-hook (lambda ()(org-babel-tangle)) nil t);-*-
- 
-;; #+title:  Straight Module
-;; #+author: Thomas Freeman
-;; #+date: Time-stamp: <2024-12-31 17:37:49 thomas>
-;; #+description: This org-mode file contains an org-babel implementation of the .emacs or .emacs.d/init.el file.
-;; #+language: en_US
-;; #+property: header-args :results silent :exports code
-
-;; #+options: html-link-use-abs-url:nil html-postamble:auto
-;; #+options: html-preamble:t html-scripts:nil html-style:t
-;; #+options: html5-fancy:nil tex:t num:nil 
-;; #+html_doctype: xhtml-strict
-;; #+html_container: div
-;; #+html_content_class: content
-;; #+keywords: Emacs
-;; #+html_link_home: index.html
-;; #+html_link_up: index.html
-
-;; #+SETUPFILE: https://tfree87.github.io/org-html-themes/org/theme-readtheorg.setup
-;; -----
-
-;; Use [[https://github.com/raxod502/straight.el][straight.el]] to pull all the packages repositories directly.
-
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -37,6 +13,13 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
+
+
 (provide 'freemacs-straight)
 
 ;;; freemacs-straight.el ends here.
+
+(if (version< emacs-version "29")
+    (straight-use-package 'use-package))
+
+(setq straight-use-package-by-default t)
