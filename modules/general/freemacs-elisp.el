18ad9b8d0b2820b0a4bb29f78fe129f57ee09f78
Thomas <thomas@pidesktop>
2025-01-01 09:07:17 -0500

    Add custom general lisp functions module.

---
+++ modules/general/freemacs-elisp.el
@@ -0,0 +1,20 @@
+;; Any
+
+;; This function checks if there are any elements in the list 
+
+
+(defun any (predicate list)
+  "Returns T if any items in LIST meet the conditions of predicate function PREDICATE without modifying the list."
+  (cond
+   ((null list) nil)
+   ((funcall predicate (car list)) t)
+   (t (any predicate (cdr list)))))
+
+;; End
+
+;; Tell Emacs what package this file provides.
+
+
+(provide 'freemacs-elisp)
+
+;;; freemacs-elisp.el ends here.
