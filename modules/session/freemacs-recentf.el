1dbb0900f92c4b75c07ca573ffd3d7c578d1fae9
Thomas <thomas@pidesktop>
2025-01-01 09:36:17 -0500

    Split freemacs-session into multiple modules.

---
+++ modules/session/freemacs-recentf.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t -*-
+
+(use-package recentf
+    :straight (:type built-in)
+    :config
+    (recentf-mode 1))
+
+(provide 'freemacs-recentf)
+
+;;; freemacs-recentf.el ends here.
---
+++ modules/session/freemacs-savehist.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t -*-
+
+(use-package savehist
+  :straight (:type built-in)
+  :init
+  (savehist-mode))
+
+(provide 'freemacs-savehist)
+
+;;; freemacs-savehist.el ends here.
