25278e5f6cbfca2fe6f0d6977c8744a12184ba5d
Thomas <thomas@pidesktop>
2025-01-05 14:15:20 -0500

    Add sly module

---
+++ modules/coding/freemacs-sly.el
@@ -0,0 +1,15 @@
+;; Install Sly
+
+
+(use-package sly
+  :straight t
+  :commands sly
+  :config
+  (if (eq system-type 'windows-nt)
+      (setq inferior-lisp-program (whicher "C:/SBCL/sbcl.exe"))
+    (setq inferior-lisp-program (whicher "/usr/bin/sbcl"))))
+
+
+(provide 'freemacs-sly)
+
+;;; freemacs-sly.el ends here.
